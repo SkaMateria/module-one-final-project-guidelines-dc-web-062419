6d7645878de8971cce4c2bd506329e7b3eb729c4
SkaMateria <skamateria@gmail.com>
2019-07-09 16:01:45 -0400

    Created fighter class

---
+++ lib/Fighters.rb
@@ -0,0 +1,32 @@
+class Fighter
+
+    attr_accessor :name, :hp, :atk, :arm
+
+    @@all = []
+
+    def initialize(name, hp, atk, arm)
+        @name = name
+        @hp = hp
+        @atk = atk
+        @arm arm
+        self.class.all << self
+    end
+
+    def self.all
+        @@all
+    end
+
+    def self.create_table
+        sql = <<-SQL
+            CREATE TABLE IF NOT EXISTS fighters (
+                id INTEGER PRIMARY KEY,
+                name TEXT,
+                hp INTEGER,
+                atk INTEGER,
+                arm INTEGER
+            );
+        SQL
+        DB[:conn].execute(sql)
+
+end
+
