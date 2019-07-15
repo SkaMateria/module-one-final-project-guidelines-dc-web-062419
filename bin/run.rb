08ee6b34ac580e40f539f82ff8e6c912f85d41be
SkaMateria <skamateria@gmail.com>
2019-07-15 09:48:37 -0400

    setting up for presentation

--- bin/run.rb
+++ bin/run.rb
@@ -1,36 +1,31 @@
 require_relative '../config/environment'
 require_relative './cli.rb'
 
-# random_monster = Monster.all.sample
-# random_fighter = Fighter.all.sample
-# welcome = "Today's blood-soaked match will be......"
-# fighter_name =  random_fighter.name
-# monster_name = random_monster.name
-# fighter_hp = random_fighter.health_points
-# puts welcome
-# sleep 2.5
-# puts fighter_name
+random_monster = Monster.all.sample
+random_fighter = Fighter.all.sample
+welcome = "Today's blood-soaked match will be......"
+fighter_name =  random_fighter.name
+monster_name = random_monster.name
+fighter_hp = random_fighter.health_points
+puts welcome
+sleep 2.5
+puts fighter_name
 # puts "With HP that equals " + "#{random_fighter.health_points}"
-# sleep 1.5
-# puts "-VS-"
-# sleep 1.5
-# puts monster_name
-# sleep 2.5
-# puts "BEGIN!"
-# binding.pry
-# monster = Battle.roll_for_monster
-# fighter = Battle.roll_for_fighter
-# puts monster.name
-# sleep 1
-# puts "-VS-"
-# sleep 1
-# puts fighter.name
-# sleep 1
+sleep 1.5
+puts "-VS-"
+sleep 1.5
+puts monster_name
+sleep 2.5
+puts "BEGIN!"
 
-# Battle.declare_winner(fighter, monster)
+monster = Battle.roll_for_monster
+fighter = Battle.roll_for_fighter
+gets.chomp
 
-prompt = TTY::Prompt.new
-cli = Cli.new(prompt)
+Battle.declare_winner(fighter, monster)
 
-cli.run
+# prompt = TTY::Prompt.new
+# cli = Cli.new(prompt)
+
+# cli.run
 
